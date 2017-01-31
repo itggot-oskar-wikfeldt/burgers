2f28e22bcc4ac6c52cd92e8937f213c27b03897a
itggot-oskar-wikfeldt <oskar.wikfeldt@itggot.se>
2017-02-01 00:58:45 +0100

    asdöklascf

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,26 @@
-/**
- * Created by oskar.wikfeldt on 2017-01-31.
- */
+function toggleActive(element, state) {
+    var classList = element.classList;
+    if (state == 'active') {
+        classList.remove('inactive');
+        classList.add('active');
+    } else {
+        classList.remove('active');
+        classList.add('inactive');
+    }
+}
+
+function openMenu() {
+    var hamburger = document.querySelector('#hamburger');
+    var darkener = document.querySelector('#scrim');
+    toggleActive(hamburger, 'active');
+    toggleActive(darkener, 'active');
+}
+
+function closeMenu() {
+    var darkener = document.querySelector('#scrim');
+    if (darkener.classList.contains('active')) {
+        var hamburger = document.querySelector('#hamburger');
+        toggleActive(hamburger, 'inactive');
+        toggleActive(darkener, 'inactive');
+    }
+}
